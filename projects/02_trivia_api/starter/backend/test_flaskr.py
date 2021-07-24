9c3a9c5c5b70db7b911a2fc5f6d211947038f426
Waad199x <85866287+Waad199x@users.noreply.github.com>
2021-07-24 18:27:07 +0300

    Update test_flaskr.py

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -4,10 +4,10 @@ import json
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
-from models import setup_db, Question, Category
+from models import setup_db,Question, Category
 
 
-class TriviaTestCase(unittest.TestCase):
+class TriviaAppTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
     def setUp(self):
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}/{}".format('postgres:W3DoOoNHM@localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,15 +24,224 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
+
+        self.Adding_Question = {
+            'question' : "What is the currancy of Saudi Arabia ?",
+            'answer' : "Ryial",
+            'category' : 5,
+            'difficulty' : 3
+        }
+        self.Quiz = { "previous_questions":[],'quiz_category' : {'type': 'History' , 'id' : 4}}
+
+
+
     def tearDown(self):
         """Executed after reach test"""
         pass
 
-    """
+    """ 14/14
+    Q per page [[DONE]]
+    Q creat [[DONE]]
+    Q delete [[DONE]]
+    Category list [[DONE]]
+    Q per category [[DONE]]
+    Q search [[DONE]]
+    Quizzes [[DONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!]] 
+
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    
+  # Now for tests 
+    ''' Questions per page '''
+#Tests for displaying question endpoint 
+    # Success test for displaying questions per pages 
+    def test_get_paginated_Questions(self):
+        res = self.client().get('/Questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['Questions_list'])
+        self.assertTrue(data['total_Qs'])
+
+    # Error test for displaying questions for rquested page
+    def test_404_sent_requesting_on_unvalid_page(self):
+        respond = self.client().get('/Questions?page=2000')
+        data = json.loads(respond.data)
+
+        self.assertEqual(respond.status_code, 404)
+        self.assertEqual(data['success'],False)
+        self.assertEqual(data['message'], "resource not found") # the message should be exactly as it is in the errorhandler function
+
+
+
+
+
+    '''Question addition '''
+# Tests for adding new question
+    # success request for creating Question
+    def test_create_new_Quuestion(self):
+        res = self.client().post('/Questions/create', json=self.Adding_Question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created'])
+        self.assertTrue(len(data['Questions']))
+
+    # Failed on adding Question
+    def test_405_when_adding_Question_not_possible(self):
+        res = self.client().post('/Questions/create/45', json=self.Adding_Question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+
+
+
+
+
+    '''Question deletion'''
+# Tests for delete endpoint 
+    # succfull deleting question request 
+    # Note \ use new id for each test
+    # Tip for using the rest of the tests without this one is to change the function name from test to Test 
+    def test_deleting_Question(self):
+        res = self.client().delete('/Questions/77')
+        data = json.loads(res.data)
+
+        Q = Question.query.filter(Question.id == 77).one_or_none()
+        self.assertEqual(Q, None)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'],77)
+
+    # Deleting non existing question
+    def test_deleting_Question_that_does_not_exist(self):
+        res = self.client().delete('/Questions/2000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')    
+
+
+
+
+
+
+
+    '''Category list '''
+# Tests for Category list endpoint    
+    # Success request on getting category list
+    def test_get_categories_list(self):
+        res = self.client().get('/Questions/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'],True)
+        self.assertTrue(data['categories'])        
+
+    # Failed on getting missing categry element 
+    def test_error_for_unexisting_category_element(self):
+        res = self.client().get('/Questions/categories/0')
+        data = json.loads(res.data)
+
+        C = Category.query.filter(Category.id == 0).one_or_none()
+
+        self.assertEqual(C, None)
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'],False)
+        self.assertEqual(data['message'], 'resource not found')
+
+
+
+
+
+    '''Qusetions per category  '''
+# Tests for category endpoint 
+    # Success test for displaying questions for specific Category 
+    def test_get_categorized_Questions(self):
+        respond = self.client().get('/Categories/1/Questions')
+        data = json.loads(respond.data)
+        
+        self.assertEqual(data['success'],True)
+        self.assertEqual(respond.status_code, 200)
+        self.assertEqual(data['current_category'],1)
+        self.assertTrue(data['total_Qs'])
+
+    # Failed request for displaying questions for unexisting Category 
+    def test_error_get_categorized_Questions_unexisting_category(self):
+        respond = self.client().get('/Categories/55/Questions')
+        data = json.loads(respond.data)
+        
+        self.assertEqual(data['success'],False)
+        self.assertEqual(respond.status_code, 404)
+        self.assertEqual(data['message'], 'resource not found')
+
+
+
+
+
+    '''  Qusetions search'''
+# Tests for search results
+    # succesful search attempt
+    def test_search(self):
+        res = self.client().post('/Questions/search', json={'searchTerm': 'what'} )
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['Questions'])
+        self.assertTrue(data['total_Qs'])
+        self.assertTrue(data['current_category'])
+
+    # giving error for specific char
+    def test_error_search(self):
+        res = self.client().post('/Questions/search', json={'searchTerm': ','} )
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')    
+        
+
+
+
+    '''Quizzes'''
+# playing game 
+    # checking if post request is succeful
+    def test_successful_quizzes(self):
+
+        res = self.client().post('/quizzes', json = self.Quiz)
+        data = json.loads(res.data)
+
+       
+
+        self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+
+        self.assertTrue(data['question'])
+
+    # Error for non for non-existing quiz page
+    def test_quizzes(self):
+        res = self.client().post('/quizzes?page=4')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+
+
+
+
+
+
 
 
 # Make the tests conveniently executable
