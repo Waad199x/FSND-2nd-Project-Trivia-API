7419b1a93069b0317b1f8232fe1060bab99fd4e9
Waad199x <85866287+Waad199x@users.noreply.github.com>
2021-07-25 11:24:08 +0300

    Update test_flaskr.py

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -14,9 +14,14 @@ class TriviaAppTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgresql://{}/{}".format('postgres:W3DoOoNHM@localhost:5432', self.database_name)
-        setup_db(self.app, self.database_path)
+
+        self.DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
+        self.DB_USER = os.getenv('DB_USER', 'postgres')  
+        self.DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')  
+        self.DB_NAME = os.getenv('DB_NAME', 'trivia_test')  
+        self.DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(self.DB_USER, self.DB_PASSWORD, self.DB_HOST, self.DB_NAME)
+
+        setup_db(self.app, self.DB_PATH)
 
         # binds the app to the current context
         with self.app.app_context():
@@ -235,14 +240,6 @@ class TriviaAppTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'resource not found')
-
-
-
-
-
-
-
-
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
